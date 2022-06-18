b73551a7712a56c20902e06b94610736b2e22d5e
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-06-18 21:41:02 +0600

    .

---
+++ hackerrank/SQL/1. Basic Select/1. Revising the Select Query I.sql
@@ -0,0 +1,4 @@
+-- Problem: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+SELECT	*
+FROM	CITY
+WHERE	POPULATION > 100000 AND COUNTRYCODE = 'USA';
--- hackerrank/SQL/1. Revising the Select Query I.sql
+++
@@ -1,4 +0,0 @@
--- Problem: https://www.hackerrank.com/challenges/revising-the-select-query/problem
-SELECT  *
-FROM    CITY
-WHERE   POPULATION > 100000 AND COUNTRYCODE = 'USA';
