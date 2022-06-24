15ac35d369d4fdf774b2d7baed8845ad2af945a6
Saif Shahriar <89329547+saifshahriar@users.noreply.github.com>
2022-06-24 13:45:53 +0600

    Added more problems

--- hackerrank/C/2. Conditionals and Loops/2. For Loop in C.c
+++ hackerrank/C/2. Conditionals and Loops/2. For Loop in C.c
@@ -2,23 +2,13 @@
 
 int main() {
     int a, b;
-    scanf("%d\n%d", &a, &b);
-    for (int i = a; i >= a && i <= b; i++) {
-        switch (i) {
-            case 1:     printf("one\n");    break;
-            case 2:     printf("two\n");    break;
-            case 3:     printf("three\n");  break;
-            case 4:     printf("four\n");   break;
-            case 5:     printf("five\n");   break;
-            case 6:     printf("six\n");    break;
-            case 7:     printf("seven\n");  break;
-            case 8:     printf("eight\n");  break;
-            case 9:     printf("nine\n");   break;
-            default:    printf("%s\n", (i % 2 == 0) ? "even" : "odd");
-        }
-    }
+    char* g[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight","nine",};
 
+    scanf("%d\n%d", &a, &b);
+    
+    for (int i = a; i >= a && i <= b; i++) {
+        printf("%s\n", (i >= 1 && i <= 9) ? g[i-1] : (i % 2 == 0) ? "even" : "odd");
+    }
+    
     return 0;
 }
-
-// const char* g[] = {"one", "two", "three", "four", "five", "six", "seven", "eight" "nine",}
