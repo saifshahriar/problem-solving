aaa93a05c2a70e13d32b45644a607818535f1301
Saif Shahriar <89329547+saifshahriar@users.noreply.github.com>
2022-06-24 14:03:51 +0600

    Added more problems

--- hackerrank/C++/1. Introduction/3. Conditional Statements.cpp
+++ hackerrank/C++/1. Introduction/3. Conditional Statements.cpp
@@ -4,17 +4,8 @@ using namespace std;
 
 int main() {
     int n; cin>>n;
-    switch (n) {
-        case 1:     cout<<"one"<<endl;      break;
-        case 2:     cout<<"two"<<endl;      break;
-        case 3:     cout<<"three"<<endl;    break;
-        case 4:     cout<<"four"<<endl;     break;
-        case 5:     cout<<"five"<<endl;     break;
-        case 6:     cout<<"six"<<endl;      break;
-        case 7:     cout<<"seven"<<endl;    break;
-        case 8:     cout<<"eight"<<endl;    break;
-        case 9:     cout<<"nine"<<endl;     break;
-        default:    cout<<"Greater than 9"<<endl;
-    }
+    string index[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    string res = (n <= 9) ? index[n-1] : "Greater than 9";
+    cout<<res<<endl;
     return 0;
 }
