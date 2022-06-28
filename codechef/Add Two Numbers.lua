78d1ec0d3989daeb943f56ed85c552da18892aad
Saif Shahriar <89329547+saifshahriar@users.noreply.github.com>
2022-06-28 19:37:54 +0600

    Added more problems

---
+++ codechef/Add Two Numbers.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/env lua
+
+local t = tonumber(io.read())
+
+for _ = 1, t do
+    local a, b = io.read("*number", "*number")
+    print(a + b)
+end
---
+++ codechef/Volume Control.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int t;
+    int x, y;
+    
+    scanf("%d", &t);
+    
+    for(int i = 0; i < t; i++) {
+        scanf("%d %d", &x, &y);
+        printf("%d\n", abs(x-y));
+    }
+    
+	return 0;
+}
