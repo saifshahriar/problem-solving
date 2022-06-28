cd2b3f33ba5f66485f6ab78ccf619580c273236b
Saif Shahriar <89329547+saifshahriar@users.noreply.github.com>
2022-06-28 20:20:37 +0600

    Added more problems

---
+++ codechef/Credit score.lua
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+
+local x = tonumber(io.read())
+
+print(x >= 750 and "YES" or "NO")
---
+++ codechef/Enormous Input Test.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/env lua
+
+local n, k = io.read("*number", "*number")
+local count = 0
+
+for _ = 1, n do
+    local num = io.read("*number")
+    if num % k == 0 then
+        count = count + 1
+    end
+end
+
+print(count)
