6b6b480c9b8aa528cb33367fd8cb27662081f752
Saif Shahriar <sh.saif@proton.me>
2025-02-06 19:33:35 +0600

    Add more problems

---
+++ beecrowd/2753_output_7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	for (char chr = 'a'; chr <= 'z'; ++chr)
+		printf("%d e %c\n", chr, chr);
+    return 0;
+}
---
+++ beecrowd/2754_output_8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	double a = 234.345,
+	       b = 45.698;
+
+	printf("%0.6lf - %0.6lf\n", a, b);
+	printf("%0.0lf - %0.0lf\n", a, b);
+	printf("%0.1lf - %0.1lf\n", a, b);
+	printf("%0.2lf - %0.2lf\n", a, b);
+	printf("%0.3lf - %0.3lf\n", a, b);
+	printf("%0.6lfe+02 - %0.6lfe+01\n", a / 100, b / 10);
+	printf("%0.6lfE+02 - %0.6lfE+01\n", a / 100, b / 10);
+	printf("%0.3lf - %0.3lf\n", a, b);
+	printf("%0.3lf - %0.3lf\n", a, b);
+
+    return 0;
+}
