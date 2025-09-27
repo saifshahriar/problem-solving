4de1329e9b50e194d418f5c3999573cb8d16fd39
Saif Shahriar <sh.saif@proton.me>
2025-09-28 04:32:01 +0600

    add algorithms directory

---
+++ algorithms/basics/kadane.cpp
@@ -0,0 +1,29 @@
+/* kadane's algorithm
+ * used is problems where you need to work with `maximum subarray sum`
+ *
+ * general idea: iterate and find current sum, if current sum is
+ * negative, make it 0. find the maximum sum.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long kadane(int n, vector<int>& v) {
+	int currsum = 0, maxsum = LLONG_MIN;
+	for (int i = 0; i < n; ++i) {
+		currsum = v[i];
+		maxsum  = max(maxsum, currsum);
+		if (currsum < 0)
+			currsum = 0;
+	}
+	return maxsum;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int         n      = 8;
+	vector<int> v      = { -1, 3, -2, 5, 3, -5, 2, 2 };
+	long long   maxsum = kadane(n, v);
+	cout << maxsum << '\n';
+}
